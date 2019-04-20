7a49316756dc2afc05984f23b2184cd1a093df8e
daudmu21 <daudmuhudin@gmail.com>
2019-04-20 17:06:07 -0500

    add instance

--- instance.tf
+++ instance.tf
@@ -3,6 +3,11 @@ resource "aws_instance" "web" {
    instance_type = "${var.instance_type}"
    count = "${var.count}"
    key_name = "${var.key_name}"
+   subnet_id = "${aws_subnet.dev.id}"
+   associate_public_ip_address = "true"
+   vpc_security_group_ids = "{aws_security_group.allow.ssh.id}"
+   
+   
    tags {
       Name = "terraform-january-${var.Created_by}-${count.index +1}"
       Env = "${var.Env}"
