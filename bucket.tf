c0efe11cf2f04970686f4b434a07413b3f2ed7b5
daudmu21 <daudmuhudin@gmail.com>
2019-04-25 17:12:08 -0500

    Edited bucket

--- bucket.tf
+++ bucket.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "newbucket" {
+resource "aws_s3_bucket" "newbucket1" {
   bucket = "terrofrom-january-daud1"
   tags { 
       Name = "terrofrom-january-daud1"
